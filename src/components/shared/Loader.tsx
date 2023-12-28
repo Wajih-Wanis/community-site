d8363ffe4d7b060450564552d1707a551efc2d64
Wajih-Wanis <wajihwanis2000@gmail.com>
2023-12-28 11:44:26 +0100

    Fixed loader path

--- src/components/shared/Loader.tsx
+++ src/components/shared/Loader.tsx
@@ -4,7 +4,7 @@ const Loader = () => {
   return (
     <div className="flex-center w-full">
       <img
-      src="/assests/icons/loader.svg"
+      src="/assets/icons/loader.svg"
       alt="loader"
       width={24}
       height={24}
