c7b47a12d532d63ab3b7379bf3d1411ad7146907
Wajih-Wanis <wajihwanis2000@gmail.com>
2024-01-14 18:55:45 +0100

    Implemented mutations for posts saving

--- src/lib/react-query/queries.tsx
+++ src/lib/react-query/queries.tsx
@@ -5,7 +5,7 @@ import{
     useQueryClient,
     useInfiniteQuery,
 } from '@tanstack/react-query'
-import { approvePost, createPost, createUserAccount, getRecentPosts, signInAccount, signOutAccount } from '../appwrite/api'
+import { approvePost, createPost, createUserAccount, deleteSavedPost, getRecentPosts, savePost, signInAccount, signOutAccount } from '../appwrite/api'
 import { INewPost, INewUser } from '@/types'
 import SigninForm from '@/_auth/forms/SigninForm'
 import { QUERY_KEYS } from './queryKeys';
@@ -72,3 +72,43 @@ export const useApprovePost = () => {
       }
   })
 }
+
+export const useSavePost = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({ postId, userId }: {postId: string, userId: string }
+      ) => savePost(postId, userId), 
+      onSuccess: () => {
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_RECENT_POSTS] 
+        })
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_POSTS] 
+        })
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_CURRENT_USER] 
+        })
+      }
+  })
+}
+
+export const useDeleteSavedPost = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (savedRecordId: string) => deleteSavedPost(savedRecordId), 
+      onSuccess: () => {
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_RECENT_POSTS] 
+        })
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_POSTS] 
+        })
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_CURRENT_USER] 
+        })
+      }
+  })
+}
+
