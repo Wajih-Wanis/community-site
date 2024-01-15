0bc6255f4ff14fa5c5072a803c0ccb476d8b0246
Wajih-Wanis <wajihwanis2000@gmail.com>
2024-01-15 13:36:20 +0100

    Fixed typo

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -32,7 +32,7 @@ const PostStats = ({post, userId}: PostStatsProps) => {
         <img src={`${checkIsApproved(approves,userId) ? "/assets/icons/likes.svg" : "/assets/icons/like.svg"}  `} 
         alt='approve' width={20} height={20} 
         onClick={handleApprovePost} className='cursor-pointer' />
-        <p className='small-medium lg:base-medium'><{approves.length}/p>
+        <p className='small-medium lg:base-medium'>{approves.length}</p>
       </div>
       <div className='flex gap-2 mr-5'>
         <img src='/assets/icons/save.svg' alt='approve' width={20} height={20} onClick={() => {}} className='cursor-pointer' />
