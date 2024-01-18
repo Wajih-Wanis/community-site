cd22f97257cb78b1c5d0e8f903d2c6b59a16f59d
Wajih-Wanis <wajihwanis2000@gmail.com>
2024-01-18 17:45:03 +0100

    Added loader to post saving

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -1,6 +1,7 @@
 import { useApprovePost, useDeleteSavedPost, useGetCurrentUser, useSavePost } from '@/lib/react-query/queries';
 import { checkIsApproved} from '@/lib/utils';
 import { Models } from 'appwrite';
+import { Loader } from 'lucide-react';
 import React, { useEffect, useState } from 'react'
 
 type PostStatsProps = {
@@ -45,11 +46,11 @@ const PostStats = ({post, userId}: PostStatsProps) => {
 
 
   const {mutate: approvePost} = useApprovePost();
-  const {mutate: savePost} = useSavePost();
-  const {mutate: deleteSavedPost} = useDeleteSavedPost();
+  const {mutate: savePost, isPending: isSavingPost } = useSavePost();
+  const {mutate: deleteSavedPost, isPending: isDeletingSaved} = useDeleteSavedPost();
 
   const {data: currentUser} = useGetCurrentUser();
-  const savedPostRecord = currentUser?.save.find((record: Models.Document) => record.$id === post.$id);
+  const savedPostRecord = currentUser?.save.find((record: Models.Document) => record.post.$id === post.$id);
 
   useEffect(() => {
     setIsSaved(!!savedPostRecord)
@@ -64,8 +65,8 @@ const PostStats = ({post, userId}: PostStatsProps) => {
         <p className='small-medium lg:base-medium'>{approves.length}</p>
       </div>
       <div className='flex gap-2 mr-5'>
-        <img src={isSaved ? 'assets/icons/saved.svg' : '/assets/icons/save.svg'}   alt='save' 
-        width={20} height={20} onClick={handleSavePost} className='cursor-pointer' />
+      {isSavingPost || isDeletingSaved ? <Loader /> : <img src={isSaved ? 'assets/icons/saved.svg' : '/assets/icons/save.svg'}   alt='save' 
+        width={20} height={20} onClick={handleSavePost} className='cursor-pointer' /> }
       </div>
     </div>
   )
