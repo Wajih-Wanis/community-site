c91ffadb63044a1dbba873608b1e702fbc48df34
Wajih-Wanis <wajihwanis2000@gmail.com>
2024-01-20 10:36:56 +0100

    Started implementing post details page

--- src/_root/pages/PostDetails.tsx
+++ src/_root/pages/PostDetails.tsx
@@ -1,6 +1,10 @@
+import { useGetPostById } from '@/lib/react-query/queries'
 import React from 'react'
+import { useParams } from 'react-router-dom';
 
 const PostDetails = () => {
+  const {id} = useParams()
+  const {data: post, isPending} = useGetPostById(id || '');
   return (
     <div>
       
